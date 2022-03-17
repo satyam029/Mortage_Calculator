3143394e017c06d435edfcb2f5fa14e0c9112603
Satyam gupta <64685701+satyam029@users.noreply.github.com>
2022-03-17 13:36:06 +0530

    Add files via upload

---
+++ Mortage_Calculator/src/com/satyam/Main.java
@@ -0,0 +1,14 @@
+package com.satyam;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int principal = (int) Console.readNumber("Principal: ", 1000, 1_000_000);
+        float annualInterest = (float) Console.readNumber("Annual Interest Rate: ", 1, 30);
+        byte years = (byte) Console.readNumber("Period (Years): ", 1, 30);
+
+        MortgageReport.printMortgage(principal, annualInterest, years);
+        MortgageReport.printPaymentSchedule(principal, annualInterest, years);
+    }
+
+}
---
+++ Mortage_Calculator/src/com/satyam/MortgageReport.java
@@ -0,0 +1,27 @@
+package com.satyam;
+
+import java.text.NumberFormat;
+
+public class MortgageReport {
+    private static MortgageCalculator calculator;
+
+    public static void printMortgage(int principal, float annualInterest, byte years) {
+        calculator= new MortgageCalculator(principal,annualInterest,years);
+        double mortgage = calculator.calculateMortgage();
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println();
+        System.out.println("MORTGAGE");
+        System.out.println("--------");
+        System.out.println("Monthly Payments: " + mortgageFormatted);
+    }
+
+    public static void printPaymentSchedule(int principal, float annualInterest, byte years) {
+        System.out.println();
+        System.out.println("PAYMENT SCHEDULE");
+        System.out.println("----------------");
+        for (short month = 1; month <= years * MortgageCalculator.MONTHS_IN_YEAR; month++) {
+            double balance = calculator.calculateBalance(month);
+            System.out.println(NumberFormat.getCurrencyInstance().format(balance));
+        }
+    }
+}
